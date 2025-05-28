625752e324bb74fca92bbc42a5ccca8a6a36469f
stedman <stedmancolinr@gmail.com>
2025-05-28 13:26:08 -0700

    ywa

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/StratosTesting.java
@@ -0,0 +1,20 @@
+package com.example.meepmeeptesting;
+
+import static java.lang.Math.toRadians;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.*;
+import com.acmerobotics.roadrunner.trajectory.constraints.*;
+import java.io.*;
+import java.util.Arrays;
+import java.util.function.*;
+import javax.imageio.ImageIO;
+import org.rowlandhall.meepmeep.MeepMeep;
+import org.rowlandhall.meepmeep.roadrunner.*;
+import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import org.rowlandhall.meepmeep.roadrunner.trajectorysequence.TrajectorySequence;
+
+public class StratosTesting {
+
+    public static class AutoConstants {}
+}
---
+++ Ptechnodactyl/src/main/java/org/firstinspires/ftc/ptechnodactyl/commands/paths.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.ptechnodactyl.commands;
+
+import com.technototes.library.command.Command;
+import com.technototes.library.command.ParallelCommandGroup;
+import com.technototes.library.command.WaitCommand;
+import com.technototes.path.command.TrajectorySequenceCommand;
+import org.firstinspires.ftc.ptechnodactyl.AutoConstants;
+import org.firstinspires.ftc.ptechnodactyl.Robot;
+import org.firstinspires.ftc.ptechnodactyl.commands.ArmCommand;
+import org.firstinspires.ftc.ptechnodactyl.commands.ClawCmds;
+
+public class paths {}

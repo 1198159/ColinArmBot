2d807d70b96ed7825a6e8d650b9e0429f8e54538
KOOLPOOL <141171610+Kooolpool@users.noreply.github.com>
2025-05-28 17:39:32 -0700

    sample thingis

--- Ptechnodactyl/src/main/java/org/firstinspires/ftc/ptechnodactyl/AutoConstants.java
+++ Ptechnodactyl/src/main/java/org/firstinspires/ftc/ptechnodactyl/AutoConstants.java
@@ -3,41 +3,66 @@ package org.firstinspires.ftc.ptechnodactyl;
 import static java.lang.Math.toRadians;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.trajectory.Trajectory;
-import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
+import com.technototes.path.geometry.ConfigurablePoseD;
+import com.technototes.path.trajectorysequence.TrajectorySequence;
+import com.technototes.path.trajectorysequence.TrajectorySequenceBuilder;
 
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 public class AutoConstants {
 
-    public static Function<Pose2d, TrajectoryBuilder> func;
-    public static Pose2d START = new Pose2d(35, 66, toRadians(180));
-    public static Pose2d SCORE = new Pose2d(55, 55, toRadians(225));
-    public static Pose2d INTAKE1 = new Pose2d(48, 38, toRadians(270));
-    public static Pose2d INTAKE2 = new Pose2d(58, 38, toRadians(270));
-    public static Pose2d INTAKE3 = new Pose2d(58, 26, toRadians(0));
-    public static Pose2d TRAVEL = new Pose2d(40, 13, toRadians(0));
-    public static Pose2d ASCENTL1 = new Pose2d(23.6, 11, toRadians(0));
-    public static Pose2d TEST9 = new Pose2d(1, 38, toRadians(0));
-    public static final Supplier<Trajectory> START_TO_SCORE_ = () ->
-            func.apply(START).lineToLinearHeading(SCORE).build();
-    public static final Supplier<Trajectory> SCORE_TO_INTAKE1 = () ->
-            func.apply(SCORE).lineToLinearHeading(INTAKE1).build();
-    public static final Supplier<Trajectory> INTAKE1_TO_SCORE = () ->
-            func.apply(INTAKE1).lineToLinearHeading(SCORE).build();
+    public static ConfigurablePoseD START = new ConfigurablePoseD(35, 66, toRadians(180));
+    public static ConfigurablePoseD SCORE = new ConfigurablePoseD(55, 55, toRadians(225));
+    public static ConfigurablePoseD INTAKE1 = new ConfigurablePoseD(48, 38, toRadians(270));
+    public static ConfigurablePoseD INTAKE2 = new ConfigurablePoseD(58, 38, toRadians(270));
+    public static ConfigurablePoseD INTAKE3 = new ConfigurablePoseD(58, 26, toRadians(0));
+    public static ConfigurablePoseD TRAVEL = new ConfigurablePoseD(40, 13, toRadians(0));
+    public static ConfigurablePoseD ASCENTL1 = new ConfigurablePoseD(23.6, 11, toRadians(0));
+    public static final Function<Function<Pose2d, TrajectorySequenceBuilder>, TrajectorySequence> START_TO_SCORE = func ->
+            func
+                    .apply(START.toPose())
+                    .lineToLinearHeading(SCORE.toPose())
+                    .build();
+    public static final Function<Function<Pose2d, TrajectorySequenceBuilder>, TrajectorySequence> SCORE_TO_INTAKE1 = func ->
+            func
+                    .apply(SCORE.toPose())
+                    .lineToLinearHeading(INTAKE1.toPose())
+                    .build();
+    public static final Function<Function<Pose2d, TrajectorySequenceBuilder>, TrajectorySequence> INTAKE1_TO_SCORE = func ->
+            func
+                    .apply(INTAKE1.toPose())
+                    .lineToLinearHeading(SCORE.toPose())
+                    .build();
 
-    public static final Supplier<Trajectory> SCORE_TO_INTAKE2 = () ->
-            func.apply(SCORE).lineToLinearHeading(INTAKE2).build();
-    public static final Supplier<Trajectory> INTAKE2_TO_SCORE = () ->
-            func.apply(INTAKE2).lineToLinearHeading(SCORE).build();
-    public static final Supplier<Trajectory> SCORE_TO_INTAKE3 = () ->
-            func.apply(SCORE).lineToLinearHeading(INTAKE3).build();
-    public static final Supplier<Trajectory> INTAKE3_TO_SCORE = () ->
-            func.apply(INTAKE3).lineToLinearHeading(SCORE).build();
-    public static final Supplier<Trajectory> SCORE_TO_TRAVEL = () ->
-            func.apply(SCORE).lineToLinearHeading(TRAVEL).build();
-    public static final Supplier<Trajectory> TRAVEL_TO_ASCENTL1 = () ->
-            func.apply(TRAVEL).lineToLinearHeading(ASCENTL1).build();
+    public static final Function<Function<Pose2d, TrajectorySequenceBuilder>, TrajectorySequence> SCORE_TO_INTAKE2 = func ->
+            func
+                    .apply(SCORE.toPose())
+                    .lineToLinearHeading(INTAKE2.toPose())
+                    .build();
+    public static final Function<Function<Pose2d, TrajectorySequenceBuilder>, TrajectorySequence> INTAKE2_TO_SCORE = func ->
+            func
+                    .apply(INTAKE2.toPose())
+                    .lineToLinearHeading(SCORE.toPose())
+                    .build();
+    public static final Function<Function<Pose2d, TrajectorySequenceBuilder>, TrajectorySequence> SCORE_TO_INTAKE3 = func ->
+            func
+                    .apply(SCORE.toPose())
+                    .lineToLinearHeading(INTAKE3.toPose())
+                    .build();
+    public static final Function<Function<Pose2d, TrajectorySequenceBuilder>, TrajectorySequence> INTAKE3_TO_SCORE = func ->
+            func
+                    .apply(INTAKE3.toPose())
+                    .lineToLinearHeading(SCORE.toPose())
+                    .build();
+    public static final Function<Function<Pose2d, TrajectorySequenceBuilder>, TrajectorySequence> SCORE_TO_TRAVEL = func ->
+            func
+                    .apply(SCORE.toPose())
+                    .lineToLinearHeading(TRAVEL.toPose())
+                    .build();
+    public static final Function<Function<Pose2d, TrajectorySequenceBuilder>, TrajectorySequence> TRAVEL_TO_ASCENTL1 = func ->
+            func
+                    .apply(TRAVEL.toPose())
+                    .lineToLinearHeading(ASCENTL1.toPose())
+                    .build();
 
 }
--- Ptechnodactyl/src/main/java/org/firstinspires/ftc/ptechnodactyl/commands/paths.java
+++ Ptechnodactyl/src/main/java/org/firstinspires/ftc/ptechnodactyl/commands/paths.java
@@ -4,34 +4,35 @@ import com.technototes.library.command.Command;
 import com.technototes.library.command.ParallelCommandGroup;
 import com.technototes.library.command.WaitCommand;
 import com.technototes.path.command.TrajectorySequenceCommand;
-import com.technototes.path.trajectorysequence.TrajectorySequence;
 
 import org.firstinspires.ftc.ptechnodactyl.AutoConstants;
 import org.firstinspires.ftc.ptechnodactyl.Robot;
-import org.firstinspires.ftc.ptechnodactyl.commands.ArmCommand;
-import org.firstinspires.ftc.ptechnodactyl.commands.ClawCmds;
 import org.firstinspires.ftc.ptechnodactyl.subsystems.ArmSubsystem;
-import org.firstinspires.ftc.ptechnodactyl.subsystems.ClawSubsystem;
-import org.firstinspires.ftc.ptechnodactyl.subsystems.DrivebaseSubsystem;
-import com.technototes.library.command.SequentialCommandGroup;
 
 public class paths {
 
     public static Command SampleScoring(Robot r) {
-public static TrajectorySequence {
-    return new TrajectorySequenceCommand(r.drivebaseSubsystem, AutoConstants. START_TO_SCORE_)
-            .alongWith((new ArmCommand(r.armSubsystem,ArmSubsystem.ArmConstants.SCORE,r.armSubsystem::wristUp,r.clawSubsystem::pivotneutral,r.clawSubsystem::closeClaw)
-            .andThen(new WaitCommand(0.1))
-            .andThen(ClawCmds.cmds.OpenClaw(r.clawSubsystem))
-                    .andThen(new WaitCommand(0.15))
-                    .andThen()
-            .addTrajectory(AutoConstants.SCORE_TO_INTAKE1.get())
-            .addTrajectory(AutoConstants.INTAKE1_TO_SCORE.get())
-            .addTrajectory(AutoConstants.SCORE_TO_INTAKE2.get())
-            .addTrajectory(AutoConstants.INTAKE2_TO_SCORE.get())
-            .addTrajectory(AutoConstants.SCORE_TO_INTAKE3.get())
-            .addTrajectory(AutoConstants.INTAKE3_TO_SCORE.get())
-            .addTrajectory(AutoConstants.SCORE_TO_TRAVEL.get())
-            .addTrajectory(AutoConstants.TRAVEL_TO_ASCENTL1.get())
-            .build();
+        return new TrajectorySequenceCommand(r.drivebaseSubsystem, AutoConstants.START_TO_SCORE)
+                .alongWith(new ParallelCommandGroup(
+                        new ArmCommand(r.armSubsystem, ArmSubsystem.ArmConstants.SCORE),
+                        r.armSubsystem::wristUp,
+                        r.clawSubsystem::pivotneutral,
+                        r.clawSubsystem::closeClaw))
+                .andThen(new WaitCommand(0.1))
+                .andThen(ClawCmds.cmds.OpenClaw(r.clawSubsystem))
+                .andThen(new WaitCommand(0.15))
+                .andThen(new TrajectorySequenceCommand(r.drivebaseSubsystem, AutoConstants.SCORE_TO_INTAKE1))
+
+                .andThen(new TrajectorySequenceCommand(r.drivebaseSubsystem, AutoConstants.INTAKE1_TO_SCORE))
+                .andThen(new TrajectorySequenceCommand(r.drivebaseSubsystem, AutoConstants.SCORE_TO_INTAKE2))
+                .andThen(new TrajectorySequenceCommand(r.drivebaseSubsystem, AutoConstants.SCORE_TO_INTAKE2))
+                .andThen(new TrajectorySequenceCommand(r.drivebaseSubsystem, AutoConstants.INTAKE2_TO_SCORE))
+                .andThen(new TrajectorySequenceCommand(r.drivebaseSubsystem, AutoConstants.SCORE_TO_INTAKE3))
+                .andThen(new TrajectorySequenceCommand(r.drivebaseSubsystem, AutoConstants.INTAKE3_TO_SCORE))
+                .andThen(new TrajectorySequenceCommand(r.drivebaseSubsystem, AutoConstants.SCORE_TO_TRAVEL))
+                .andThen(new TrajectorySequenceCommand(r.drivebaseSubsystem, AutoConstants.TRAVEL_TO_ASCENTL1));
+
+
+
+    }
 }
